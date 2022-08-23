a2b667f343a3b5984ab6bc3b8842275ec1124bc9
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-08-22 21:07:27 -0300

    se empezo algo

---
+++ js/almacenar.js
@@ -0,0 +1,12 @@
+const AGREGAR = document.getElementById("agregar");
+const lista = [];
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    AGREGAR.addEventListener("click", ()=>{
+        let item = document.getElementById("item").value;
+        
+
+
+    })
+})
