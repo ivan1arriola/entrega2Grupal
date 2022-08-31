43adcb3798848af4e86df5b24c578093f121a181
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-08-31 19:16:59 -0300

    arregle ifs

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,48 +1,41 @@
 const AGREGAR = document.getElementById("agregar");
 
-
 document.addEventListener("DOMContentLoaded", () => {
+  actualizar();
+  AGREGAR.addEventListener("click", () => {
+    let item = document.getElementById("item").value;
+    agregarLocalStorage(item.trim()); //  trim( ) elimina los espacios en blanco en ambos extremos del string
     actualizar();
-    AGREGAR.addEventListener("click", () => {
-        let item = document.getElementById("item").value;
-        agregarLocalStorage(item.trim()); //  trim( ) elimina los espacios en blanco en ambos extremos del string
-        actualizar();
-    });
+  });
 });
 
-
 function actualizar() {
-    let lista = document.getElementById("contenedor");
-    lista.innerHTML = "";
-    let array = JSON.parse(localStorage.getItem("array"));
-    if (array != null)
-        array.map(item => {
-            lista.innerHTML += `<li>${item}</li>`;
-        });
+  let lista = document.getElementById("contenedor");
+  lista.innerHTML = "";
+  let array = JSON.parse(localStorage.getItem("array"));
+  if (array != null)
+    array.map((item) => {
+      lista.innerHTML += `<li>${item}</li>`;
+    });
 }
 
 function agregarLocalStorage(parametro) {
-    if (parametro === "") {
+  if (parametro !== "") {
+    if (localStorage.getItem("array") === null) {
+      localStorage.setItem("array", JSON.stringify([parametro]));
+    } else {
+      let array = JSON.parse(localStorage.getItem("array"));
+      array.push(parametro);
+      localStorage.setItem("array", JSON.stringify(array));
     }
-    else{
-
-        if (localStorage.getItem("array") === null) {
-            localStorage.setItem("array", JSON.stringify([parametro]));
-        } else {
-            let array = JSON.parse(localStorage.getItem("array"));
-            array.push(parametro);
-            localStorage.setItem("array", JSON.stringify(array));
-        }
-
-
-    }
+  }
 }
+
 limpiar.addEventListener("click", () => {
-    limpiarItems();
+  limpiarItems();
 });
 
 function limpiarItems() {
-    localStorage.removeItem("array");
-    actualizar();
+  localStorage.removeItem("array");
+  actualizar();
 }
-
